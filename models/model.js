e2f482a092f693efbc8a37b2a63b44c55a288c27
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-06-26 16:02:18 +0100

    add missed refactor

--- models/model.js
+++ models/model.js
@@ -1,15 +1,5 @@
 const db = require("../db/connection");
 
-
-exports.fetchDocumentation = () => {
-    return fs.readFile("./endpoints.json", "utf8", (err, data) => {
-        return data
-    })
-    .then((data) => {
-        return JSON.parse(data)
-    })
-}
-
 exports.fetchTopics = () => {
     return db.query(`SELECT * FROM topics;`).then((data) => {
         return data.rows;
