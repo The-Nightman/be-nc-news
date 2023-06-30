b7d17f4f3e8a7035125e9241d63e13af224a846f
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-06-30 08:38:13 +0100

    add delete api/comments/commment_id test suite

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -317,4 +317,34 @@ describe("patch /api/articles/:article_id", () => {
     });
 });
 
+describe("delete /api/comments/:comment_id", () => {
+    test.only("returns status 204 without content and deletes comment by the given id when passed a valid id", () => {
+        return request(app)
+            .delete("/api/comments/1")
+            .expect(204)
+            .then(({ body }) => {
+                expect(body).toEqual({});
+                
+            });
+    });
+    test.only("returns status 404 when given a valid but non-existing comment id", () => {
+        return request(app)
+            .delete("/api/comments/32")
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.message).toBe('Comment does not exist!');
+                
+            });
+    });
+    test.only("returns status 400 when given an invalid search is performed", () => {
+        return request(app)
+            .delete("/api/comments/bad")
+            .expect(400)
+            .then(({ body }) => {
+                expect(body.message).toBe('Bad request! Enter a valid ID');
+                
+            });
+    });
+});
+
 afterAll(() => db.end());
